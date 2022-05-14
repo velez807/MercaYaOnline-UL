680d31185033690c20132eae98d054e11cf9b645
Juan Sebastian Velez <sebasdelvelez@gmail.com>
2022-05-14 17:50:58 -0500

    Interfaces v1.0

--- index.py
+++ index.py
@@ -10,5 +10,18 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route('/register.html')
+def register():
+    return render_template('register.html')
+
+@app.route('/terminarRegistro.html')
+def terminarRegistro():
+    return render_template('terminarRegistro.html')
+
+@app.route('/mercaya.html')
+def mercaya():
+    return render_template('mercaya.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
