6a5d0d500f1cdde8195e198a76d62fd8bd75366b
Juan Sebastian Velez <sebasdelvelez@gmail.com>
2022-05-15 19:25:12 -0500

    interfaces v4.0

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def home():
